dd8a42813c7a660a47fca147eedc513d165271ff
user <alimuradof93@gmail.com>
2025-07-08 14:58:33 +0400

    user information has been added

---
+++ auth-server/src/main/java/az/turing/authserver/entity/Role.java
@@ -0,0 +1,5 @@
+package az.turing.authserver.entity;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ auth-server/src/main/java/az/turing/authserver/entity/User.java
@@ -0,0 +1,42 @@
+package az.turing.authserver.entity;
+
+import jakarta.persistence.*;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Email
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
