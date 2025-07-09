c123623e61d0fcc2a299729ea365722db697652d
user <alimuradof93@gmail.com>
2025-07-09 19:43:48 +0400

    authentication has been added

---
+++ auth-server/src/main/java/az/turing/authserver/auth/JwtTokenProvider.java
@@ -0,0 +1,56 @@
+package az.turing.authserver.auth;
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private String secretKey = "SuperSecretKeyForJwtTokenGeneration12345";
+    private final long validityInMilliseconds = 3600000;
+
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        this.key = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public String generateToken(String email) {
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setSubject(email)
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return getClaims(token).getSubject();
+    }
+
+    public boolean isValid(String token) {
+        try {
+            getClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
---
+++ auth-server/src/main/java/az/turing/authserver/service/AuthService.java
@@ -0,0 +1,52 @@
+package az.turing.authserver.service;
+
+import az.turing.authserver.auth.JwtTokenProvider;
+import az.turing.authserver.dto.AuthResponse;
+import az.turing.authserver.dto.LoginRequest;
+import az.turing.authserver.dto.RegisterRequest;
+import az.turing.authserver.entity.Role;
+import az.turing.authserver.entity.User;
+import az.turing.authserver.exception.AuthenticationException;
+
+import az.turing.authserver.repo.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    public AuthResponse register(RegisterRequest request){
+        User user=User.builder()
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+        userRepository.save(user);
+        String token = jwtTokenProvider.generateToken(user.getEmail());
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+    }
+    public AuthResponse login(LoginRequest request){
+        User user= userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new AuthenticationException("User not found"));
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())){
+            throw new AuthenticationException("Invalid password");
+        }
+        String token = jwtTokenProvider.generateToken(user.getEmail());
+
+        return AuthResponse.builder()
+                .token(token)
+                .build();
+
+    }
+
+}
