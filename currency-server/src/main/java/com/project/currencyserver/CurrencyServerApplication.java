327fa3a472e2693e40131d44cfb53b137277dac7
user <alimuradof93@gmail.com>
2025-07-24 18:11:56 +0400

    updating tural branch

--- currency-server/src/main/java/com/project/currencyserver/CurrencyServerApplication.java
+++ currency-server/src/main/java/com/project/currencyserver/CurrencyServerApplication.java
@@ -2,8 +2,10 @@ package com.project.currencyserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class CurrencyServerApplication {
 
     public static void main(String[] args) {
--- currency-server/src/main/java/com/project/currencyserver/repository/CurrencyRepository.java
+++ currency-server/src/main/java/com/project/currencyserver/repository/CurrencyRepository.java
@@ -2,9 +2,10 @@ package com.project.currencyserver.repository;
 
 import com.project.currencyserver.model.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface CurrencyRepository extends JpaRepository<Currency, Long> {
     Optional<Currency> findBysourceCurrencyAndtargetCurrency(String from, String to);
 }
